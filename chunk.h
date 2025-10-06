fe19d8ec996778bb29ccb9c69e007a2943ac491f
M-H-T-I <hassantariqirshad@gmail.com>
2025-10-06 19:34:55 +0500

    create OpCode enum with first instruction

--- chunk.h
+++ chunk.h
@@ -1,10 +1,13 @@
 //module to define our code representation
-
-
 #ifndef clox_chunk_h
 #define clox_chunk_h
 
 #include "common.h"
 
+// all possible bytecode instructions like the ones in assembly
+typedef enum{
+    OP_RETURN, // return value from current function
+} OpCode; 
 
-#endif
+
+#endif 
