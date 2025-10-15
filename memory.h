883354222b4335bd3b777a21cb1785f266e9db10
MHT-ALT <l250653@lhr.nu.edu.pk>
2025-10-15 08:33:56 +0500

    fix for grow array

--- memory.h
+++ memory.h
@@ -7,7 +7,7 @@
 #define GROW_CAPACITY(capacity)  ((capacity) < 8 ? 8: (capacity)*2)
 
 // calls reallocate
-#define GROW_ARRAY(type, pointer,oldCount, newCount) (type*)reallocate(pointer, sizeof(type)*oldcount, sizeof(type) * newCount)
+#define GROW_ARRAY(type, pointer,oldCount, newCount) (type*)reallocate(pointer, sizeof(type)*oldCount, sizeof(type) * newCount)
 
 void* reallocate(void* pointer, size_t oldSize, size_t newSize);
 
